0a7eec729f0e970ae421a3a2c67b38bd0ca0791b
BHANU REKHA <BHANU REKHA@DESKTOP-KM2GMSP>
2020-07-24 18:15:11 +0530

    My Epam Task8

---
+++ EPAMTask8/src/com/task8/EpamHTask8/First2CharsRemove.java
@@ -0,0 +1,17 @@
+package com.task8.EpamHTask8;
+public class First2CharsRemove 
+{
+
+	public String delete(String str) 
+	{
+		StringBuilder sb=new StringBuilder(str);
+		String finalstring;
+		if(str.charAt(0)=='A'&&str.charAt(1)=='A')
+			sb=sb.delete(0, 2);
+		else if(str.charAt(0)=='A'||str.charAt(1)=='A')
+			sb=sb.deleteCharAt(str.indexOf('A'));
+		finalstring=sb.toString();
+		return finalstring;
+	}
+
+}
